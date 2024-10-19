884dfbf6b8383e29bdaa618e31d0dedae69bf3cc
Dushyant Bhutyani <dushyantb2003@gmail.com>
2024-10-19 12:39:22 +0530

    Add password hasing and generate token functionality

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 const userSchema = new Schema({
     username: {
@@ -69,5 +71,49 @@ const userSchema = new Schema({
 }, {
     timestamps: true
 })
+
+// Pre-hook to hash password
+userSchema.pre("save", function (next) {
+    if (!this.isModified("password"))
+        return next();
+
+    this.password = bcrypt.hash(this.password, 10)
+    next();
+})
+
+// Some methods
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+console.log("In user model", process.env.ACCESS_TOKEN_SECRET)
+
+userSchema.methods.generateAccessToken = async function () {
+    return jwt.sign(
+        // Create a payload
+        {
+            id: this._id,
+            email: this.email,
+            username: this.username
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = async function () {
+    return jwt.sign(
+        // Create a payload
+        {
+            id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
 export const User = mongoose.model("User", userSchema)
