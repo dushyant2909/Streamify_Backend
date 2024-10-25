c7982b5aab7c14511dd4f0d4e66a9625e300bb1e
Dushyant Bhutyani <dushyantb2003@gmail.com>
2024-10-25 22:19:57 +0530

    Add delete video from cloudinary functionality in delete video controller

--- src/utils/deleteFileFromCloudinary.js
+++ src/utils/deleteFileFromCloudinary.js
@@ -1,9 +1,11 @@
 import { v2 as cloudinary } from 'cloudinary'
 import ApiError from './ApiError.js'
 
-const deleteFileFromCloudinary = async (publicId) => {
+const deleteFileFromCloudinary = async (publicId, file = "image") => {
     try {
-        await cloudinary.uploader.destroy(publicId)
+        await cloudinary.uploader.destroy(publicId,
+            { resource_type: file }
+        )
     } catch (error) {
         console.log("Error in deleting file form cloudinary::", error.message || error)
         throw new ApiError(401, error.message)
