ab65b2dd4ccb45ab05209730c4e36f5c27d0bbc6
Dushyant Bhutyani <dushyantb2003@gmail.com>
2024-10-29 12:11:27 +0530

    Add edit comment functionaity

--- src/routes/commentRoutes.js
+++ src/routes/commentRoutes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/authMiddleware.js";
-import { createComment } from "../controllers/commentController.js";
+import { createComment, editComment } from "../controllers/commentController.js";
 
 const commentRoute = Router();
 
 commentRoute.use(verifyJWT)
 
 commentRoute.route('/create/:videoId').post(createComment)
+commentRoute.route('/edit/:commentId').patch(editComment)
 
 export default commentRoute
